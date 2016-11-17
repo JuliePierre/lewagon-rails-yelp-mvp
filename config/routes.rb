9a3be62c18554da95ad3482faf77ca867c5b85b7
Julie Pierre <julie.pierre3@gmail.com>
2016-11-17 10:19:52 +0100

    exit

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,11 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  resources :restaurants, only: [:index, :show, :new, :create] do
-    resources :reviews, only: [:new, :create]
+  namespace :admin do
+    resources :restaurants, only: [:index]
   end
+
+  resources :restaurants, only: [:index, :show, :new, :create] do
+    resources :reviews, only: [:create]
+  end
+
 end
